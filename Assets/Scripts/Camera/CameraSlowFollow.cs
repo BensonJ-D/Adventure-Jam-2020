b3163066b3b89ae8ad0190e2b31927a82506bab0
Sage Stainsby <sage.stainsbys@gmail.com>
2020-04-30 09:52:23 +0000

    Adding portal and smooth follow.

---
+++ Assets/Scripts/Camera/CameraSlowFollow.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraSlowFollow : MonoBehaviour
+{
+    public GameObject target;
+
+    private void Update()
+    {
+        gameObject.transform.position = Vector3.MoveTowards(
+            new Vector3(gameObject.transform.position.x,
+            0, -10f),
+            new Vector3(target.transform.position.x,
+            0, -10f), 3 * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Misc/Portal.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public string destinationScene;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.name == "Player")
+        {
+            SceneManager.LoadScene(destinationScene);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireCube(transform.position, new Vector3(1f,3f,1f));
+    }
+}
